8ef87c92ff4532bf51f27dc8de63c9693238cef2
ChildOfJustice <izzvms@gmail.com>
2021-04-10 11:33:48 +0300

    My own equation for the ellipses distribution.

--- Task_3/WPF Control Elements/Spinner/CircularProgressBar.xaml.cs
+++ Task_3/WPF Control Elements/Spinner/CircularProgressBar.xaml.cs
@@ -5,7 +5,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Task_3.WPF_Control_Elements.Spinner
 {
@@ -66,23 +65,16 @@ namespace Task_3.WPF_Control_Elements.Spinner
         }
 
         private ObservableCollection<Ellipse> _ellipses;
-        
-        // private readonly DispatcherTimer _animationTimer;
-        
-        
+
+
         public CircularProgressBar()
         {
             InitializeComponent();
             
             Loaded += OnCanvasLoaded;
             
-            IsVisibleChanged += OnVisibleChanged;
+            //IsVisibleChanged += OnVisibleChanged;
 
-            // _animationTimer = new DispatcherTimer(DispatcherPriority.ContextIdle, Dispatcher)
-            // {
-            //     Interval = new TimeSpan(0, 0, 0, 0, 175)
-            // };
-            
             InitEllipses();
             
         }
@@ -102,91 +94,23 @@ namespace Task_3.WPF_Control_Elements.Spinner
 
         private static void SetPosition(DependencyObject ellipse, double x, double y)
         {
-            //50 + 
             ellipse.SetValue(Canvas.LeftProperty, x);
             ellipse.SetValue(Canvas.TopProperty, y);
-           
         }
-        private void Start()
-        {
-            // _animationTimer.Tick += OnAnimationTick;
-            // _animationTimer.Start();
-        }
-        private void Stop()
-        {
-            // _animationTimer.Stop();
-            // _animationTimer.Tick -= OnAnimationTick;
-        }
-        private void OnAnimationTick(object sender, EventArgs e)
-        {
-            // _transformAngle = (_transformAngle + 36) % 360;
-            // _ellipses.Add(new Ellipse());
-            // _ellipses = null;
-            //System.Windows.MessageBox.Show(_transformAngle.ToString());
-            //_spinnerRotate.Angle = (_spinnerRotate.Angle + 36) % 360;
-            //_spinnerRotate2.Angle = (_spinnerRotate2.Angle + 36) % 360;
-        }
-
         
-
         private void InitEllipses()
         {
             _ellipses = new ObservableCollection<Ellipse>();
-           
-            // const double offset = Math.PI;
-            // double step = Math.PI * 2 / (double)(CirclesQuantity + 1);
-            // double opacityStep = 1.0 / CirclesQuantity;
-            // // var ellipse = new Ellipse();
-            // // ellipse.Width = 20;
-            // // ellipse.Height = 20;
-            // // ellipse.Stretch = Stretch.Fill;
-            // // ellipse.Fill = new SolidColorBrush(Color.FromRgb(100,20,250));
-            // // Ellipses.Add(ellipse);
-            // //
-            //
-            // for (int i = 0; i < CirclesQuantity; i++)
-            // {
-            //     var ellipse = new Ellipse();
-            //     Canvas.SetLeft(ellipse, 0);
-            //     ellipse.Width = CirclesSize;
-            //     ellipse.Height = CirclesSize;
-            //     ellipse.Stretch = Stretch.Fill;
-            //     ellipse.Fill = new SolidColorBrush(Color.FromRgb(100,20,250));
-            //     Ellipses.Add(ellipse);
-            //     
-            //     
-            //     // var newCircle = new Ellipse();
-            //     // newCircle.Width = CirclesSize;
-            //     // newCircle.Height = CirclesSize;
-            //     // newCircle.SetValue(Canvas.LeftProperty, 0);
-            //     // newCircle.SetValue(Canvas.TopProperty, 0);
-            //     // newCircle.Stretch = Stretch.Fill;
-            //     // newCircle.Opacity = 1.0 - i * opacityStep;
-            //     // SetPosition(newCircle, offset, i, step);
-            //     // Ellipses.Add(newCircle);
-            // }
-
-
-
-
-
-
-
         }
-
-        // private void OnMainCanvasLoaded(object sender, RoutedEventArgs e)
-        // {
-        //     ItemsControl.SetValue(Canvas.TopProperty, -60);
-        //     ItemsControl.SetValue(Canvas.LeftProperty, -60);
-        // }
+        
         private void OnCanvasLoaded(object sender, RoutedEventArgs e)
         {
-
             
             double ActualSize = Size;
+            double opacityStep = 1.0 / CirclesQuantity;
             for (int i = 0; i < CirclesQuantity; i++)
             {
-                _ellipses.Add(new Ellipse {Height = ActualSize, Width = ActualSize, Fill = Color});
+                _ellipses.Add(new Ellipse {Height = ActualSize, Width = ActualSize, Fill = Color, Opacity = opacityStep*i});
                 //ActualSize -= Size / CirclesQuantity;
             }
 
@@ -198,112 +122,17 @@ namespace Task_3.WPF_Control_Elements.Spinner
             // int xCenter = 20;
             // int yCenter = 20;
             
-            
             int dt = 30 / Speed;
             double dphi = 1;
             if (!ClockDirection) dphi *= -1;
 
+            int startX = Radius;
+            int startY = 0;
+            double phi = 0;
             foreach (int i in Enumerable.Range(0, _ellipses.Count))
             {
-                double phi = 0;
-                _ellipses[i].Margin = GetThicknessForCirclePath(xCenter, yCenter, Radius, phi + i * CircleShift);
-                
-                phi %= 360;
-                // DispatcherTimer timer = new DispatcherTimer();
-                // timer.Tick += new EventHandler((sender2, e2) =>
-                // {
-                //     elipses[i].Margin = GetThicknessForCirclePath(xCenter, yCenter, Radius, phi + i * CircleShift);
-                //     phi += dphi;
-                //     phi %= 360;
-                // });
-                // timer.Interval = new TimeSpan(0, 0, 0, 0, dt);
-                // timer.Start();
+                _ellipses[i].Margin = GetThicknessForCirclePath(startX, startY, xCenter, yCenter,phi + i * CircleShift);
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            //
-            // double ActualSize = Size;
-            // for (int i = 0; i < CirclesQuantity; i++)
-            // {
-            //     _ellipses.Add(new Ellipse {Height = ActualSize, Width = ActualSize, Fill = Color});
-            //     //ActualSize -= Size / CirclesQuantity;
-            // }
-            //
-            // double CircleShift = (360) / (CirclesQuantity);
-            //
-            //
-            //
-            //
-            //
-            //
-            //
-            //
-            // int xCenter = -CirclesSize;
-            // int yCenter = -CirclesSize;
-            //
-            //
-            // int dt = 30 / Speed;
-            // double dphi = 1;
-            // if (!ClockDirection) dphi *= -1;
-            // double phi = 0;
-            // foreach (int i in Enumerable.Range(0, _ellipses.Count))
-            // {
-            //     
-            //     _ellipses[i].Margin = GetThicknessForCirclePath(xCenter, yCenter, Radius, phi + i * CircleShift);
-            //     phi += dphi;
-            //     phi %= 360;
-            //     // timer.Interval = new TimeSpan(0, 0, 0, 0, dt);
-            //     // timer.Start();
-            // }
-            //const double offset = Math.PI;
-            //double step = Math.PI * 2 / (double)(CirclesQuantity + 1);
-            // double step = 360 / (double)(CirclesQuantity );
-            // double opacityStep = 1.0 / CirclesQuantity;
-            //
-            //
-            // //MessageBox.Show("Hello, world!", "My App");
-            // double i = 0;
-            // double x = 100;
-            // double y = 0;
-            // foreach (var ellipse in Ellipses)
-            // {
-            //     // ellipse.SetValue(Canvas.LeftProperty, 0);
-            //     // ellipse.SetValue(Canvas.TopProperty, 0);
-            //     //SetPosition(ellipse, x, y, i, step);
-            //     i++;
-            // }
-            
-            
-            // SetPosition(_circle0, offset, 0.0, step);
-            // SetPosition(_circle1, offset, 1.0, step);
-            // SetPosition(_circle2, offset, 2.0, step);
-            // SetPosition(_circle3, offset, 3.0, step);
-            // SetPosition(_circle4, offset, 4.0, step);
-            // SetPosition(_circle5, offset, 5.0, step);
-            // SetPosition(_circle6, offset, 6.0, step);
-            // SetPosition(_circle7, offset, 7.0, step);
-            // SetPosition(_circle8, offset, 8.0, step);
         }
 
         private void OnEllipseCanvasLoaded(object sender, RoutedEventArgs e)
@@ -313,32 +142,16 @@ namespace Task_3.WPF_Control_Elements.Spinner
                 SetPosition(_ellipses[i], -(Radius + Size/2), -(Radius + Size/2));
             }
         }
-        
-        private void OnCanvasUnloaded(object sender, RoutedEventArgs e)
-        {
-            Stop();
-        }
-        
-        private void OnVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            var isVisible = (bool)e.NewValue;
 
-            if (isVisible)
-            {
-                Start();
-            }
-            else
-            {
-                Stop();
-            }
-        }
-        
-        private Thickness GetThicknessForCirclePath(int xCenter, int yCenter, int radius, double phi)
+        private Thickness GetThicknessForCirclePath(int x, int y,int xCenter, int yCenter, double phi)
         {
-            double x = radius * Math.Cos(phi / 180.0 * Math.PI) + xCenter;
-            double y = radius * Math.Sin(phi / 180.0 * Math.PI) + yCenter;
+            // double x = radius * Math.Cos(phi / 180.0 * Math.PI) + xCenter;
+            // double y = radius * Math.Sin(phi / 180.0 * Math.PI) + yCenter;
+            phi = phi / 180.0 * Math.PI;
+            double _x = (Math.Cos(phi)*x - Math.Sin(phi)*y) + xCenter;
+            double _y = (Math.Sin(phi)*x + Math.Cos(phi)*y) + yCenter;
             
-            return new Thickness(x, y, 0, 0);
+            return new Thickness(_x, _y, 0, 0);
         }
     }
 }
