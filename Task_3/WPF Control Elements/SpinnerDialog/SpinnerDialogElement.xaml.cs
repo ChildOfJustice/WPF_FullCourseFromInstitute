87634139a91f64e399122441071688a13b9217ba
ChildOfJustice <izzvms@gmail.com>
2021-04-16 10:29:22 +0300

    Working on the spinner dialog.

---
+++ Task_3/WPF Control Elements/SpinnerDialog/SpinnerDialogElement.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Task_3.WPF_Control_Elements.Spinner;
+
+namespace Task_3.WPF_Control_Elements.SpinnerDialog
+{
+    public partial class SpinnerDialogElement : UserControl
+    {
+        public SpinnerDialogElement()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("!!!!@");
+            //DialogHost.WorkSpaceBorder.CornerRadius = new CornerRadius(100,0,0,0);
+            //DialogHost.WorkSpaceBorder.DataContext = null;
+            
+            
+            
+            // MessageBox.Show("COMMAND");
+            // ContentWindow win = new ContentWindow();
+            // if (owner != null)
+            //     win.Owner = owner;
+            //var vm = new CircularProgressBar();
+            //win.DataContext = vm;
+            // win.ShowDialog();
+
+            //DO NOT WORK
+            //DialogHost.WorkSpaceBorder.DataContext = new Content();
+        }
+    }
+}
