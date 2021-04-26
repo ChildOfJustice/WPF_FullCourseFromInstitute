b6b82f2f5e51325945172a06626e45c6a2e63864
ChildOfJustice <izzvms@gmail.com>
2021-04-26 19:45:37 +0300

    Trying the shit.

---
+++ Task_3/WPF Styles/AdjustedScrollViewer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Task_3.WPF_Styles
+{
+    public class AdjustedScrollViewer// : ScrollViewer
+    {
+        public static readonly DependencyProperty ThumbColorProperty = DependencyProperty.RegisterAttached(
+            "ThumbColor", typeof(Brush), typeof(AdjustedScrollViewer));
+        // public static readonly DependencyProperty ThumbColorProperty = DependencyProperty.RegisterAttached(
+        //     "ThumbColor",
+        //     typeof(Color),
+        //     typeof(AdjustedScrollViewer),
+        //     new PropertyMetadata(Color.FromRgb(220, 55, 100))
+        // );
+        
+        public static void SetThumbColor(UIElement element, Brush value)
+        {
+            element.SetValue(ThumbColorProperty, value);
+            MessageBox.Show("!!!");
+        }
+        public static Brush GetThumbColor(UIElement element)
+        {
+            MessageBox.Show("GET");
+            return (Brush)element.GetValue(ThumbColorProperty);
+            
+        }
+
+        // public AdjustedScrollViewer()
+        // {
+        //     Loaded += OnLoad;
+        // }
+        
+        // private void OnLoad(object sender, RoutedEventArgs e)
+        // {
+        //     MessageBox.Show("!!!!");
+        //     
+        // }
+    }
+}
