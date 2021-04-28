35beaeaa6ea811d7378fd590229899b9692303ee
ChildOfJustice <izzvms@gmail.com>
2021-04-28 15:33:06 +0300

    added keyboards.

---
+++ Task_3/WPF Control Elements/ScreenKeyBoard/ScreenKeyBoardElement.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Task_3.WPF_Control_Elements.ScreenKeyBoard
+{
+    public partial class ScreenKeyBoardElement : UserControl
+    {
+        public ScreenKeyBoardElement()
+        {
+            InitializeComponent();
+        }
+        
+        private void _btnAlphaKeyboard_Click(object sender, RoutedEventArgs e)
+        {
+            Keyboard.KeyboardState = Rife.Keyboard.KeyboardState.AlphaNumeric;
+        }
+
+        private void _btnNumericKeyboard_Click(object sender, RoutedEventArgs e)
+        {
+            Keyboard.KeyboardState = Rife.Keyboard.KeyboardState.Numeric;
+        }
+    }
+}
